0d1910098723a0b3f3588831115ee1e096194ddc
Akshay More <moreakshay725@gmail.com>
2025-07-28 20:19:23 +0530

    first commit

---
+++ screenshot.js
@@ -0,0 +1,14 @@
+const puppeteer = require("puppeteer");
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto("https://console.cloud.google.com/monitoring/dashboards", {
+    waitUntil: "networkidle2",
+  });
+
+  // TODO: Automate login if not already logged in (see Note below).
+  await page.screenshot({ path: "dashboard.png", fullPage: true });
+
+  await browser.close();
+})();
---
+++ send_whatsapp.js
@@ -0,0 +1,34 @@
+const { Client, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
+const fs = require("fs");
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", async () => {
+  const chatName = "YOUR GROUP NAME HERE";
+  const filePath = "./dashboard.png";
+  const caption = "all is well";
+
+  const chats = await client.getChats();
+  const group = chats.find((chat) => chat.isGroup && chat.name === chatName);
+
+  if (group) {
+    const media = await client.sendMessage(
+      group.id._serialized,
+      fs.readFileSync(filePath),
+      { caption }
+    );
+    console.log("Screenshot sent to WhatsApp group!");
+  } else {
+    console.log("Group not found!");
+  }
+  client.destroy();
+});
+
+client.initialize();
