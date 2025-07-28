eb2aae1f467284429f66ed5ed9999d47710f3cc3
Akshay More <moreakshay725@gmail.com>
2025-07-28 23:23:19 +0530

    google auth problem fixed

--- monitor.js
+++ monitor.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const fs = require("fs");
 const path = require("path");
 const puppeteer = require("puppeteer");
@@ -5,15 +7,19 @@ const { Client, LocalAuth, MessageMedia } = require("whatsapp-web.js");
 const qrcode = require("qrcode-terminal");
 
 // Configurations
-// const DASHBOARD_URL = "https://akshaymore.netlify.app/";
-const DASHBOARD_URL =
-  "https://console.firebase.google.com/project/eavesdrop-dummy/analytics/app/web:YTQ4NDVlMWEtYzcyZC00OThmLTg2YmMtNTg3ZDc4YzYyOGM4/overview/reports~2Fdashboard%3Fr%3Dfirebase-overview&fpn%3D674020683550";
-const GROUP_ID = "120363421612780741@g.us";
+const DASHBOARD_URL = process.env.DASHBOARD_UR;
+const GROUP_ID = process.env.GROUP_ID;
 const CAPTION = "All is well ✅";
 const VIEWPORT = { width: 1920, height: 1080 };
 
-const SCREEN_PATH = path.join(__dirname, "myscreenshots/dashboard.png");
-const LOG_PATH = path.join(__dirname, "mylogs/custom_log.txt");
+const SCREEN_PATH = path.join(
+  __dirname,
+  process.env.SCREENSHOT_PATH || "myscreenshots/dashboard.png"
+);
+const LOG_PATH = path.join(
+  __dirname,
+  process.env.LOGS_PATH || "mylogs/custom_log.txt"
+);
 
 // log-to-file wrapper
 const logStream = fs.createWriteStream(LOG_PATH, { flags: "a" });
@@ -30,17 +36,28 @@ process.on("exit", () => logStream.end());
 (async () => {
   // take screenshot
   // const browser = await puppeteer.launch({ headless: true });
+
+  /* const browser = await puppeteer.launch({
+     headless: true,
+     userDataDir: path.join(__dirname, "chrome-profile"),
+   }); */
+
+  // added by Monica
   const browser = await puppeteer.launch({
-    // headless: true,
     headless: false,
-    userDataDir: path.join(__dirname, "chrome-profile"),
+    executablePath:
+      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+    userDataDir: "D:\\Javascript\\ondcauto\\chrome-profile",
   });
+
   const page = await browser.newPage();
   await page.setViewport(VIEWPORT);
   await page.goto(DASHBOARD_URL, { waitUntil: "networkidle2" });
 
   //added pause for google auth issue
-  await page.waitForTimeout(120000);
+  // await page.waitForTimeout(1200000);
+  // await new Promise((res) => setTimeout(res, 1200000));
+
   // addition by Monica
   // element that exists only when the dashboard finished loading
   await page.waitForSelector("canvas,iframe,div.chart");
