564537f3d95da6728da08a39b402794aa7ee93ed
Akshay More <moreakshay725@gmail.com>
2025-07-31 02:44:58 +0530

    GCP error pending

--- GCP/functions/index.js
+++ GCP/functions/index.js
@@ -7,9 +7,9 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { setGlobalOptions } = require("firebase-functions");
-const { onRequest } = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+const {setGlobalOptions} = require("firebase-functions");
+// const {onRequest} = require("firebase-functions/https");
+// const logger = require("firebase-functions/logger");
 
 // For cost control, you can set the maximum number of containers that can be
 // running at the same time. This helps mitigate the impact of unexpected
@@ -21,7 +21,7 @@ const logger = require("firebase-functions/logger");
 // functions should each use functions.runWith({ maxInstances: 10 }) instead.
 // In the v1 API, each function can only serve one request per container, so
 // this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+setGlobalOptions({maxInstances: 10});
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -38,8 +38,8 @@ admin.initializeApp();
 
 // Scheduled function to write dummy data every 2 minutes
 exports.automateDummyHit = functions.pubsub
-  .schedule("every 2 minutes")
-  .onRun((context) => {
-    const ref = admin.database().ref("hits");
-    return ref.push({ message: "Automated hit", time: Date.now() });
-  });
+    .schedule("every 2 minutes")
+    .onRun((context) => {
+      const ref = admin.database().ref("hits");
+      return ref.push({message: "Automated hit", time: Date.now()});
+    });
